1a6767563138ffaa421c26a3964c27f4419a4ab8
dinodolph <jaimedelfin@ciencias.unam.mx>
2022-09-20 19:36:46 -0500

    Principios del main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,88 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Robot arisa = new Robot(1, 1);
+    	
+        System.out.println("--Bienvenido a McBurguer--"
+				+ "\nPor registra un numero de usuario y nosotros te "
+				+ "asignaremos una mesa.");
+        
+        Scanner sc = new Scanner(System.in);
+		int id;
+		
+		while (true){
+			try {
+				String idUsuario = sc.nextLine();
+				id = Integer.parseInt(idUsuario);
+				break;
+			}catch (NumberFormatException ex){
+				System.out.println("Por favor ingrese un numero.");
+			}
+		}
+		
+		Random r = new Random();
+		int mesa = r.nextInt(4)+1;
+		 
+		System.out.println("Se le ha asignado la mesa: " + mesa);
+		
+		Robot arisa = new Robot(id, mesa);
+		
+		int opcion;
+		
+		System.out.println("Bienvenido usuario " + id + ", yo soy McCocinero"
+				+ " y estoy para atenderte."
+				+ "\n Por favor elige la opcion que deseas ejeutar.");
+		
+		do{
+			System.out.println(
+				"1.- Llamar al cocinero.\n" +
+				"2.- Atender.\n" + 
+				"3.- Cocinar.\n" + 
+				"4.- Suspenderse.\n" +
+				"0.- Terminar simulacion.\n");
+
+				while (true){
+					try {
+						String opcionUsuario = sc.nextLine();
+						opcion = Integer.parseInt(opcionUsuario);
+						break;
+					}catch (NumberFormatException ex){
+						System.out.println("Por favor elige la opcion VALIDA "
+							+"1.- Llamar al cocinero.\n" +
+							"2.- Atender.\n" + 
+							"3.- Cocinar.\n" + 
+							"4.- Suspenderse.\n" +
+							"0.- Terminar simulacion.\n");
+					}
+				}
+
+				switch(opcion){
+					case 1:
+						arisa.llamarAlCocinero();
+						break;
+
+					case 2:
+						arisa.atender();
+						break;
+
+					case 3:
+						arisa.cocinar();
+						break;
+
+					case 4:
+						arisa.suspenderse();
+						break;
+
+					case 0:
+						break;
+
+					default:
+						System.out.println("\nPor favor elige la opcion que deseas ejecutar.");
+						break;
+
+				}
+
+		}while(opcion != 0);
     }
 }
